884b113e9725c58f1f159da2ac4c5704dfcbeb9b
mmchenr1 <molly_mchenry@brown.edu>
2024-09-08 15:21:19 -0400

    merge swe-site updates

--- src/App.js
+++ src/App.js
@@ -11,14 +11,46 @@ import FromTheSky from './pages/FromTheSky'
 import work_data from './data/work.json'
 import ProjectPage from './components/ProjectPage'
 
+
 function ScrollToTop() {
   const { pathname } = useLocation();
-
   useEffect(() => {
     window.scrollTo(0, 0);
   }, [pathname]);
   return null;
 }
+
+
+// only show navbar on scroll up
+window.addEventListener('DOMContentLoaded', function() {
+  let prevScrollPos = window.pageYOffset;
+  window.addEventListener('scroll', function() {
+    const currentScrollPos = window.pageYOffset;
+    console.log(currentScrollPos)
+    var navbar = document.getElementById('navbar')
+    var progressBarContainer = this.document.getElementById('progress-bar-container')
+    if (navbar === null) console.log('can\'t find #navbar element');
+    else{
+      if(progressBarContainer !== null && currentScrollPos < 40){
+        progressBarContainer.style.paddingTop = `0px`;
+      }
+      else if (Math.abs(currentScrollPos - prevScrollPos) < 90) return;
+      else if (prevScrollPos > currentScrollPos) {
+        navbar.classList.remove('hidden');
+        if(progressBarContainer !== null) {
+          progressBarContainer.style.paddingTop = `${navbar.offsetHeight}px`;
+        }
+      }
+      else {
+        navbar.classList.add('hidden');
+        if(progressBarContainer !== null) {
+          progressBarContainer.style.paddingTop = `0px`;
+        }
+      }      
+    }
+    prevScrollPos = currentScrollPos;
+  });
+});
 
 function App() {
   return (
